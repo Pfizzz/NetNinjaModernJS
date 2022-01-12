8c45132807c9b4c1b767c171a3fb5d3721f69cc7
Michael Pfister <pfiste80@gmail.com>
2022-01-12 01:48:01 -0500

    testing regex with test() and search()

--- section7/script59.js
+++ section7/script59.js
@@ -1,3 +1,5 @@
+// 59 - SUBMIT EVENTS
+
 const form = document.querySelector('.signup-form');
 // const username = document.querySelector('#username');
 
@@ -8,3 +10,23 @@ form.addEventListener('submit', e => {
     // also works with 'name'
     console.log(form.username.value);
 })
+
+// 61 - TESTING REGEX
+const username = 'shusnp';
+const pattern = /^[a-z]{6,}$/
+
+let result = pattern.test(username);
+// console.log(result);
+if(result){
+    console.log('the regex test passed :)')
+} else {
+    console.log("regex test failed :(")
+}
+
+// test username
+let result2 = username.search(pattern);
+console.log(result2);
+// logs -1 for false the position of where the match is if true
+
+
+// 62 - BASIC FORM VALIDATION
