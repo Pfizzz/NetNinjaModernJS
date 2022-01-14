460285ce0b658e4baeb9d42fbc2bdfc5ca5c3b0e
Michael Pfister <pfiste80@gmail.com>
2022-01-13 22:14:22 -0500

    timestamp conversion, comparison

--- 11-Dates/dates.js
+++ 11-Dates/dates.js
@@ -35,7 +35,39 @@ console.log(now.toLocaleString());
 
 
 // timestamps
+
 // represented as a date by the number of milliseconds since 1/1/1970
 // allows us to compare two dates together
 console.log('timestamp', now.getTime());
 
+// to create a date in the past:
+const before = new Date('December 1 2021 7:30:59');
+const timeNow = new Date();
+
+console.log(before.getTime());
+console.log(timeNow.getTime());
+
+// so subtract the difference.
+const diff = timeNow.getTime() - before.getTime();
+
+console.log(diff);
+// difference in minutes:
+const mins = Math.round(diff / 1000 / 60);
+console.log('minutes', mins);
+// difference in hours:
+const hours = Math.round(mins / 60);
+console.log('hours', hours);
+// difference in days:
+const days = Math.round(hours / 24);
+console.log('days', days);
+// difference in years (sort of)
+const years = days / 365;
+console.log('years', years);
+
+console.log(`the blog was written ${days} days ago.`);
+
+// converting timestamps into date objects
+const timestam = 1642129816573;
+// when you pass a timestamp into the Date() object, it converts it into a date
+// prints 'Thu Jan 13 2022 22:10:16 GMT-0500 (Eastern Standard Time)'
+console.log(new Date(timestam));
