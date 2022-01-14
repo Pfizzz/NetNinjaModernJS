48480509c8f675d7cd60066f84d39274b4f3e295
Michael Pfister <pfiste80@gmail.com>
2022-01-14 13:53:15 -0500

    finished for now

--- 12-AJAX/ajax.js
+++ 12-AJAX/ajax.js
@@ -21,12 +21,14 @@ const getTodos = (callback) => {
   request.send();
 };
 
-getTodos((err, data) => {
-    console.log('callback fired');
-    // console.log(err, data);
-    if(err){
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-});
+// getTodos((err, data) => {
+//     console.log('callback fired');
+//     // console.log(err, data);
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(data);
+//     }
+// });
+
+// promise example
